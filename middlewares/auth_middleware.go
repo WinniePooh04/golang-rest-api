c0a0e032bf2143cba3233fb8a092427485cdb190
khinn.tun2 <khinn.tun2@frontiir.net>
2025-02-18 09:41:54 +0630

    Add pre shared key

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -2,18 +2,24 @@ package middlewares
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-func AuthMiddleware() gin.HandlerFunc {
+func PSKMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.GetHeader("Authorization")
-		if token == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		// Get Authorization header
+		psk := c.GetHeader("Authorization")
+
+		// Check if PSK matches the expected key
+		if psk != os.Getenv("PRE_SHARED_KEY") {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or missing Pre Shared key"})
 			c.Abort()
 			return
 		}
+
+		// Continue processing request
 		c.Next()
 	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -2,12 +2,20 @@ package routes
 
 import (
 	"go-rest-api/controllers"
+	"go-rest-api/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetUpRouter(router *gin.Engine) {
+
+	// Public Route (No authentication required)
+	router.GET("/public", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "This is a public endpoint"})
+	})
+
 	fttx := router.Group("fttx")
+	fttx.Use(middlewares.PSKMiddleware()) // Apply pre-shared key middleware
 	{
 		fttx.POST("/search", controllers.GetFTTx)
 	}
